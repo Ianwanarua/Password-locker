f05fc6622b0e32b32addc2ffd01e4cec0a452758
ianwanarua <ian.wanarua@student.moringaschool.com>
2021-10-26 11:22:44 +0300

    Updated the run file

--- run.py
+++ run.py
@@ -154,8 +154,7 @@ def main():
                             lower = string.ascii_lowercase
                             upper = string.ascii_uppercase
                             num = string.digits
-                            symbols = string.punctuation
-                            all = lower + upper + num + symbols
+                            all = lower + upper + num
                             temp = random.sample(all,length)
                             password = "".join(temp)
                             
